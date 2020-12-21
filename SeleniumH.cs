8c1811473dd08b06b72633b6dccf31bdd8893f28
VladyslavStorozhenko <68906022+VladyslavStorozhenko@users.noreply.github.com>
2020-12-21 22:57:25 +0200

    Selenium homework

---
+++ SeleniumH.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumHomework
+{
+    class SeleniumH
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Program Files (x86)\\Google\\Chrome\\Application");
+            driver.Url = "https://www.epam.com/";
+        }
+
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/nav/ul/li[5]/span[1]/a", "https://www.epam.com/about")]
+
+        public void MenuTest(string xpath, string site)
+        {
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath(xpath)).Click();
+            Assert.AreEqual(driver.Url, site);
+        }
+
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/button",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[1]",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[1]/ul/li[2]/a", 
+            "https://www.epam.com/services/consult-and-design")]
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/button",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[3]",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[3]/ul/li[1]/a",
+            "https://www.epam.com/our-work/financial-services")]
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/button",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[5]",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[5]/ul/li[2]/a",
+            "https://www.epam.com/about/newsroom")]
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/button",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[6]",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/div/nav/ul/li[6]/ul/li[3]/a",
+            "https://www.epam.com/careers/epam-without-borders")]
+
+        public void MenuDropdownTest(string xpath1, string xpath2, string xpath3, string site)
+        {
+            driver.FindElement(By.XPath(xpath1)).Click();
+            driver.FindElement(By.XPath(xpath2)).Click();
+            driver.FindElement(By.XPath(xpath3)).Click();
+            Assert.AreEqual(site, driver.Url);
+        }
+
+
+        [TestCase("//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/ul/li[3]/div/button",
+            "//*[@id=\"new_form_search\"]",
+            "//*[@id=\"wrapper\"]/div[2]/div[1]/header/div/ul/li[3]/div/div/form/button", "IASA",
+            "https://www.epam.com/search?q=IASA")]
+        public void SearchBarTest(string xpath1, string xpath2, string xpath3, string word, string site)
+        {
+            driver.FindElement(By.XPath(xpath1)).Click();
+            driver.FindElement(By.XPath(xpath2)).SendKeys(word);
+            driver.FindElement(By.XPath(xpath3)).Click();
+            Assert.AreEqual(site, driver.Url);
+        }
+
+        [TestCase("//*[@id=\"wrapper\"]/div[3]/div[1]/footer/div/div[2]/ul[1]/li[3]/a",
+            "https://www.epam.com/privacy-policy")]
+        [TestCase("//*[@id=\"wrapper\"]/div[3]/div[1]/footer/div/div[2]/ul[1]/li[4]/a",
+            "https://www.epam.com/cookie-policy")]
+        public void ButtonAtTheEndOfPageTest(string xpath1, string site)
+        {
+            var element = driver.FindElement(By.XPath(xpath1));
+            Actions action = new Actions(driver);
+            action.MoveToElement(element);
+            action.Perform();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div/div[2]/button/span")).Click();
+            element.Click();
+            Assert.AreEqual(site, driver.Url);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Vladyslav_Storozhenko.feature
@@ -0,0 +1,49 @@
+﻿Feature: EPAM testing
+
+    Scenario:Go to About section
+		Given user navigates to https://www.epam.com/
+		And user maximize the window
+		When the user clicks on "ABOUT" button
+		Then https://www.epam.com/abou is opened
+
+    Scenario: Go to Consult and Design section
+		Given the user navigates to https://www.epam.com/
+		When the user hovers over "OUR WORK"
+		And the user clicks on "Financial Services"
+		Then https://www.epam.com/our-work/financial-services is opened
+
+    Scenario: Go to Financial Services section
+		Given the user navigates to https://www.epam.com/
+		When the user hovers over "SERVICES"
+		And the user clicks on "Consult & Design"
+		Then https://www.epam.com/services/consult-and-design is opened
+
+    Scenario: Go to Newsroom section
+		Given the user navigates to https://www.epam.com/
+		When the user hovers over "ABOUT"
+		And the user clicks on "NEWSROOM"
+		Then https://www.epam.com/about/newsroom is opened
+
+    Scenario: Go to EPAM without borders section
+		Given the user navigates to https://www.epam.com/
+		When the user hovers over "CAREERS"
+		And the user clicks on "EPAM without borders"
+		Then https://www.epam.com/careers/epam-without-borders is opened
+
+    Scenario: Scroll down and go to privacy policy section
+		Given the user navigates to https://www.epam.com/
+		When the user scroll down to the end of page
+		And the user clicks on "Privacy Policy"
+		Then https://www.epam.com/cookie-policy is opened
+
+    Scenario: Scroll down and go to cookie policy section
+		Given the user navigates to https://www.epam.com/
+		When the user scroll down to the end of page
+		And the user clicks on "Cookie Policy"
+		Then https://www.epam.com/cookie-policy is opened
+
+    Scenario: Functionality of searching words option
+		Given the user navigates to https://www.epam.com/
+		And the user clicks on search
+		When the user writes 'IASA' in seach bar
+		Then results related to 'IASA' word are shown
